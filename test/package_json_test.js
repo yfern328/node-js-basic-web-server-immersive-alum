64e11ccce9573b19a22535768c2487021f5d1d3a
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-04-09 08:22:37 -0700

    Add tests

--- test/package_json_test.js
+++ test/package_json_test.js
@@ -1,19 +1,20 @@
 const expect = require('chai').expect;
  const packageJson = require('../package.json');
- 
+
  describe('package.json', () => {
   it('has the correct dependencies', () => {
-    expect(Object.keys(packageJson.dependencies)).to.eql([
-      'router'
+    expect(Object.keys(packageJson.dependencies)).to.include.members([
+      'router',
+      'finalhandler'
     ]);
   });
 
   it('has the correct devDependencies', () => {
-    expect(Object.keys(packageJson.devDependencies)).to.eql([
+    expect(Object.keys(packageJson.devDependencies)).to.include.members([
       'chai',
       'mocha',
-      'mocha-multi'
+      'mocha-multi',
+      'supertest'
     ]);
   });
 });
-
---
+++ test/server_test.js
@@ -0,0 +1,130 @@
+"use strict"; 
+
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
+const request = require('supertest');
+const crypto = require('crypto');
+
+const server = require('../server');
+const baseUrl = 'http://localhost:3000';
+const salt = "6X[TU:O(zVTR|qg|SYYD64W:<BqC~V/jHi]yN8Y!uym)+LMD9_p!yRM3EU*u$4Jp";
+
+const decrypt = (encryptedTxt) => {
+  const decipher = crypto.createDecipher('aes-256-ctr', salt);
+  let decrypted = decipher.update(encryptedTxt, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+};
+
+describe('server', () => {
+
+  before(() => {
+    server.listen(3000);
+  });
+
+  it('GET request to / responds with "Hello, World!"', (done) => {
+    request(baseUrl)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', 'text/plain; charset=utf-8')
+      .end((error, response) => {
+        if (error) {
+          done(error);
+          return;
+        }
+        response.text.should.equal("Hello, World!");
+        done();
+      });
+    });
+
+    it('POST request to /messsage with message data returns message id', (done) => {
+      request(baseUrl)
+        .post('/message')
+        .send({message: "This is a test message."})
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .end((error, response) => {
+          if (error) {
+            done(error);
+            return;
+          }
+          let result = JSON.parse(response.text);
+          result.should.be.a('number');
+          done();
+        });
+    });
+
+    it('GET request to /messages returns a list of all the messages', (done) => {
+      request(baseUrl)
+        .get('/messages')
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .end((error, response) => {
+          if (error) {
+            done(error);
+            return;
+          }
+          let result = JSON.parse(response.text);
+          result.should.be.a('Array');
+          result[0].should.eql({id: 1, message: "This is a test message."});
+          done();
+        })
+    });
+
+    it('GET request to /message/:id returns the message matching the id', (done) => {
+      request(baseUrl)
+        .get('/message/1')
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .end((error, response) => {
+          if (error) {
+            done(error);
+            return;
+          }
+          let result = JSON.parse(response.text);
+          result.should.be.a('object');
+          result.should.eql({id: 1, message: "This is a test message."});
+          done();
+        });
+    });
+
+    it('GET request to /message/:id?encrypt=true returns the message encyrpted', (done) => {
+      request(baseUrl)
+        .get('/message/1?encrypt=true')
+        .expect(200)
+        .expect('Content-Type', 'text/plain; charset=utf-8')
+        .end((error, response) => {
+          if (error) {
+            done(error);
+            return;
+          }
+          let result = JSON.parse(decrypt(response.text));
+          result.should.be.a('object');
+          result.should.eql({id: 1, message: "This is a test message."});
+          done();
+        });
+    });
+
+    it('GET request to /messages?encrypt=true returns all the messages encrypted', (done) => {
+      request(baseUrl)
+        .get('/messages?encrypt=true')
+        .expect(200)
+        .expect('Content-Type', 'text/plain; charset=utf-8')
+        .end((error, response) => {
+          if (error) {
+            done(error);
+            return;
+          }
+          let result = JSON.parse(decrypt(response.text));
+          result.should.be.a('Array');
+          result.should.eql([{id:1, message: "This is a test message."}]);
+          done();
+        });
+    });
+
+  after(() => {
+    server.close();
+  });
+
+});
