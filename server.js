230fcc6abb02b5dc848b7078767ae32efac70ed3
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-04-09 09:09:12 -0700

    Add base setup of server

---
+++ server.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const http         = require('http');
+const finalhandler = require('finalhandler');
+const urlParser    = require('url');
+const querystring  = require('querystring');
+const Router       = require('router');
+
+const router = new Router();
+
+router.get('/', (request, response) => {
+  // A good place to start!
+  response.end();
+});
+
+const server = http.createServer((request, response) => {
+  router(request, response, finalhandler(request, response));
+});
+
+exports.listen = function(port, callback) {
+  server.listen(port, callback);
+};
+
+exports.close = function(callback) {
+  server.close(callback);
+};
+
